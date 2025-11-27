a82f04769c990cd421572fa12697229bb8cb1acf
Igor Lukic <igorlukic2003@gmail.com>
2025-11-27 14:21:40 +0100

    Exclude private methods from the ParserService interface

--- infrastructure/microservices/parser-microservice/src/Domain/services/IParserService.ts
+++ infrastructure/microservices/parser-microservice/src/Domain/services/IParserService.ts
@@ -3,10 +3,8 @@ import { Event } from "../models/Event";
 import { ParserEvent } from "../models/ParserEvent";
 
 export interface IParserService {
-    normalizeAndSaveEvent(message: string): Promise<EventDTO>;
+    normalizeAndSaveEvent(eventMessage: string): Promise<EventDTO>;
     getAll(): Promise<ParserEvent[]>;
     getParserEventById(id: number): Promise<ParserEvent>;
     deleteById(id: number): Promise<boolean>;
-    normalizeEvent(message: string): Promise<Event>;
-    llmAnalysis(message: string): Promise<EventDTO>;
 }
