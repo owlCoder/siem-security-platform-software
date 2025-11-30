1e7650262d9fe6fcdce262bb2ccae364aedbfded
Dijana Stojkovic <dijana.stojkovic83@gmail.com>
2025-11-30 19:53:18 +0100

    Initial implementation of Elastic search-like search

--- infrastructure/microservices/query-microservice/src/Services/QueryService.ts
+++ infrastructure/microservices/query-microservice/src/Services/QueryService.ts
@@ -1,15 +1,54 @@
 import { IQueryService } from "../Domain/services/IQueryService";
+import { IQueryRepositoryService } from "../Domain/services/IQueryRepositoryService";
+import { Event } from "../Domain/models/Event";
+import { parseQueryString } from "../Utils/ParseQuery";
 
 export class QueryService implements IQueryService {
     constructor(
+        private readonly queryRepositoryService: IQueryRepositoryService,
     ) {}
 
-    searchEvents(query: string): Promise<any[]> {
-        // TODO:
-        // prvo pozovemo getAllEvents iz IQueryRepositoryService
-        // onda filtriramo rezultate na osnovu query stringa
-        // i na kraju vracamo filtrirane rezultate
-        throw new Error("Method not implemented.");
+    async searchEvents(query: string): Promise<any[]> {
+        const allEvents = await this.queryRepositoryService.getAllEvents();
+
+        // query je npr. "type=info|date=20/10/2025" ili "type=info|host=server1|dateFrom=2025-11-20|dateTo=2025-11-22"
+        // pozivamo parseQueryString da dobijemo parove kljuc-vrednost sa nazivom polja i vrednosti za pretragu
+        const filters = parseQueryString(query);
+
+        // dodaj pre vracanja kesiranje
+        return allEvents.filter(event => {
+
+            for (const key of Object.keys(filters)) {
+                const value = filters[key].toLowerCase();
+
+                if (key === "dateFrom") {
+                    const date = new Date(filters[key]);
+                    if (new Date(event.timestamp) < date) return false;
+                    continue;
+                }
+
+                if (key === "dateTo") {
+                    const date = new Date(filters[key]);
+                    if (new Date(event.timestamp) > date) return false;
+                    continue;
+                }
+                
+                // za event proverava polja sa nazivom trenutnog key
+                // npr. event[type] ili event[host]
+                // da li sadrzi vrednost iz query stringa
+                // ako ne sadrzi takvo polje onda stavljamo da event za to polje "ima" vrednost ""
+                const eventValue = (event as any)[key]?.toString().toLowerCase() || "";
+                if (!eventValue.includes(value)) {
+                    return false;
+                }
+
+                // proveriti da li je sve pokriveno
+                // date range je pokriven
+                // a proveriti da li poslednja provera ispravno proverava sva ostala polja
+            }
+
+            return true;
+        });
     }
     
 }
